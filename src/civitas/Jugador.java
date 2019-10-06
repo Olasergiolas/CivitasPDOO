4e38fd062f0e2c7efb8665e6255ac3f665e3c19c
Sergio García <olasergiolas@gmail.com>
2019-10-06 13:56:02 +0200

    Preparadas cabeceras clase Jugador

--- src/civitas/Jugador.java
+++ src/civitas/Jugador.java
@@ -10,5 +10,193 @@ package civitas;
  * @author rullow
  */
 public class Jugador {
+    static protected int CasasMax = 4;
+    static protected int CasasPorHotel = 4;
+    protected boolean encarcelado;
+    static protected int HotelesMax = 4;
+    private String nombre;
+    private int numCasillaActual;
+    static protected float PasoPorSalida = 1000f;
+    static protected float PrecioLibertad = 200f;
+    private boolean puedeComprar;
+    private float saldo;
+    private float saldoInicial = 7500f;
+    //falta un array de títuloPropiedad llamado propiedades
+    //falta un dato de tipo Sorpresa llamado salvoconducto
     
+    Jugador(String nombre){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    protected Jugador(Jugador otro){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean cancelarHipoteca(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    int cantidadCasasHoteles(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    public int compareTo(Jugador otro){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean comprar(TituloPropiedad titulo){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean construirCasa(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean construirHotel(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    protected boolean debeSerEncarcelado(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean enBancarrota(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean encarcelar(int numCasillaCarcel){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private boolean existeLaPropiedad(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private int getCasasMax(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    int getCasasPorHotel(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private int getHotelesMax(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    protected String getNombre(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    int getNumCasillaActual(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private float getPrecioLibertad(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private float getPremioPasoSalida(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    protected TituloPropiedad[] getPropiedades(){   
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean getPuedeComprar(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    protected float getSaldo(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean hipotecar(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    public boolean isEncarcelado(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean modificarSaldo(float cantidad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean moverACasilla(int numCasilla){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean obtenerSalvoconducto(Sorpresa sorpresa){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean paga(float cantidad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean pagaAlquiler(float cantidad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean pagaImpuesto(float cantidad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean pasaPorSalida(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private void perderSalvoconducto(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean puedeComprarCasilla(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private boolean puedeSalirCarcelPagando(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private boolean puedoEdificarCasa(TituloPropiedad propiedad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private boolean puedoEdificarHotel(TituloPropiedad propiedad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    private boolean puedoGastar(float precio){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean recibe(float cantidad){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean salirCarcelPagando(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean salirCarcelTirando(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean tieneAlgoQueGestionar(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean tieneSalvoconducto(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    public String toString(){
+        throw new UnsupportedOperationException("No implementado");
+    }
+    
+    boolean vender(int ip){
+        throw new UnsupportedOperationException("No implementado");
+    }
 }
