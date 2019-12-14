f889d5df682deba69ecf205d7968e3b3f10a344c
Rullow <rullowgl@gmail.com>
2019-12-14 13:02:19 +0100

    commit para bajarme el proyecto en linux

---
+++ src/GUI/DiarioDialog.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+import civitas.Diario;
+
+/**
+ *
+ * @author ggarr
+ */
+public class DiarioDialog extends javax.swing.JDialog {
+    
+    Diario diario;
+    
+    public DiarioDialog(java.awt.Frame parent)
+    {
+        super(parent, true);
+        //initComponents();
+        setLocationRelativeTo(null);
+    }
+    
+    
+}
